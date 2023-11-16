e54aea8ae2ede617b06807d3e2dcb89191f18902
Xizzio <Xizzio@hotmail.com>
2023-11-16 08:09:58 +0000

    BUCKET_NAME now works with a enviroment variable and will create S3 bucket if not found

--- kjell/hello_world/app.py
+++ kjell/hello_world/app.py
@@ -8,6 +8,8 @@ import os
 # Instruksjoner for å kjøre ... (Kan sikkert lage container senere ..)
 #
 # pip3 install -r requirements.txt
+# export BUCKET_NAME=<you_bucket>
+# echo $BUCKET_NAME
 # python3 app.py
 #
 # Hilsen Kjell
@@ -16,9 +18,29 @@ s3_client = boto3.client('s3', region_name='eu-west-1')
 rekognition_client = boto3.client('rekognition', region_name='eu-west-1')
 
 # Oppgave 1A
-BUCKET_NAME = "kjellsimagebucker"
+BUCKET_NAME = os.environ.get('BUCKET_NAME', 'default_bucket_name')
+
+
+def create_s3_bucket(bucket_name):
+    try:
+        location_constraint = s3_client.meta.region_name  # Use the current region
+        s3_client.create_bucket(
+            Bucket=bucket_name,
+            CreateBucketConfiguration={'LocationConstraint': location_constraint}
+        )
+        print(f"Bucket '{bucket_name}' created successfully.")
+    except s3_client.exceptions.BucketAlreadyOwnedByYou as e:
+        print(f"Bucket '{bucket_name}' already exists.")
+    except Exception as e:
+        print(f"Error creating bucket '{bucket_name}': {str(e)}")
+
 
 def lambda_handler(event, context):
+    
+    
+    # Create S3 bucket if it doesn't exist
+    create_s3_bucket(BUCKET_NAME)
+
 
     # List all objects in the S3 bucket
     paginator = s3_client.get_paginator('list_objects_v2')
