a83c913684a6fe2dfdfe7d3bd91d2c18ad9cf42f
Xizzio <Xizzio@hotmail.com>
2023-11-27 20:06:42 +0000

    cpu and memory values

--- infra/main.tf
+++ infra/main.tf
@@ -4,7 +4,7 @@ resource "aws_apprunner_service" "service" {
   instance_configuration {
     instance_role_arn = aws_iam_role.role_for_apprunner_service.arn
     
-    cpu = 256
+    cpu    = 256
     memory = 1024
   }
 
